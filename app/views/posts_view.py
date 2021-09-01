86dcac7338ca5549ffa1b8121be7c91ecf9f962c
Andre Flor <andreflor96@hotmail.com>
2021-08-31 23:10:02 -0300

    fix: status code and responses

--- app/views/posts_view.py
+++ app/views/posts_view.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from pymongo import response
 
 
 from app.models.posts_model import Post
@@ -10,13 +11,14 @@ def init_app(app: Flask):
     @app.post("/posts")
     def create_post():
         data = request.json
-
-        post = Post(title=data["title"], author=data["author"], content=data["content"], tags=data["tags"])
-        response =  post.create()
-        if response:
-            return {"data": response}, 201
-
-        return {"msg": "erro"}, 400
+        try:
+            post = Post(title=data["title"], author=data["author"], content=data["content"], tags=data["tags"])
+            response =  post.create()
+            if response:
+                return {"data": response}, 201
+        except KeyError as err:
+            return {"error": "Wrong keys inserted"}, 406
+        
 
     @app.get("/posts")
     def read_posts():
@@ -26,14 +28,32 @@ def init_app(app: Flask):
     
     @app.get("/posts/<int:post_id>")
     def read_post_by_id(post_id: int):
-        post = Post.read_specifc_post(post_id=post_id)
+        response = Post.read_specifc_post(post_id=post_id)
 
-        return {"data": post}, 200
+        if response:
+            return {"data": response}, 200
+        
+        return {"error": "Post not found"}, 404
 
     @app.delete("/posts/<int:post_id>")
     def delete_post(post_id):
         response = Post.delete_post(post_id=post_id)
+
         if response:
-            return "", 204
+            return {"data": response}, 200
         
-        return {"msg": "Post not found"}, 404
+        return {"error": "Post not found"}, 404
+
+
+    @app.patch("/posts/<int:post_id>")
+    def update_post(post_id: int):
+        data: dict = request.json
+        try:
+            response = Post.update(post_id=post_id, data=data)
+            if response:
+                return {"data": response}, 200
+        
+            return {"error": "Post not found"}, 404
+            
+        except KeyError:
+            return {"error": "Wrong keys inserted"}, 406
